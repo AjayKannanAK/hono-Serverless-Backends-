95e1d5f5b1549d6b0549fa770e6481d5214e5520
Ajay Kannan A K <ajaykannanak23@gmail.com>
2024-03-14 22:13:37 +0530

    modified adding body, headers, queryparams

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,21 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// app.get('/', (c) => {
+//   //body, headers, query params, middlewares, connecting to a database
+//   return c.text('Hello Hono!')
+// })
+
+app.post("/", async(c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("authorization"));
+  console.log(c.req.query("param"));
+
+  return c.json({
+    message: "Hi there!"
+  });
+
+});
 
 export default app
