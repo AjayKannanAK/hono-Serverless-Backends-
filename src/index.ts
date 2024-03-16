ff878547fd2f4382cdb94a2dcfccc103614c82dd
Ajay Kannan A K <ajaykannanak23@gmail.com>
2024-03-16 07:18:34 +0530

    added middleware

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+//Go through slides
+
 import { Hono } from 'hono'
 
 const app = new Hono()
@@ -7,7 +9,21 @@ const app = new Hono()
 //   return c.text('Hello Hono!')
 // })
 
-app.post("/", async(c) => {
+async function authMiddleware (c: any, next: any)  {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    const timeBeforePostRequest = new Date().getTime();
+    await next(); //when you do some logic or code after the request handler(here post request) then await here or else simply return next();
+    const timeTaken = (new Date().getTime() - timeBeforePostRequest) / 1000; //calculating how much time the post request takes
+    console.log(timeTaken)
+  } else {
+    return c.text("You dont have acces");
+  }
+}
+
+//app.use(authMiddleware);
+
+app.post("/", authMiddleware, async(c) => {
   const body = await c.req.json();
   console.log(body);
   console.log(c.req.header("authorization"));
